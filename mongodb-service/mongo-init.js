0ad65a910c4eb64a0f6342f51299e9dddabeedb5
lsefiane <lyes.sefiane@gmail.com>
2022-09-27 13:55:54 -0400

    [cloud-native-network-graph-application] : Collection Creation Initialization.
    - db.createCollection("network-graph")

--- mongodb-service/mongo-init.js
+++ mongodb-service/mongo-init.js
@@ -7,6 +7,12 @@ db = db.getSiblingDB("graph")
 // Insert a log trace
 db.log.insertOne({"message": "Switched to graph Database."});
 
+// Collection created
+db.createCollection("network-graph")
+
+// Insert a log trace
+db.log.insertOne({"message": "Collection network-graph created."});
+
 // Create a user for the database above.
 // TODO : find a way to pass the credentials from Docker ENV to this script (if possible).
 db.createUser(

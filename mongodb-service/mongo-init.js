15bd00af21a721981560c29fd4cb9149db0dc91e
lsefiane <lyes.sefiane@gmail.com>
2022-09-27 11:29:10 -0400

    [mongodb-service] : Added the mongo-init.js to initialize the database creation at runtime via Docker.

---
+++ mongodb-service/mongo-init.js
@@ -0,0 +1,23 @@
+// db.getSiblingDB() : used to return another database without modifying the db variable in the shell environment.
+// You can use db.getSiblingDB() as an alternative to the use <database> helper.
+// This is particularly useful when writing scripts using mongosh where the use helper is not available.
+// Ref.: https://www.mongodb.com/docs/manual/reference/method/db.getSiblingDB/
+db = db.getSiblingDB("graph")
+
+// Insert a log trace
+db.log.insertOne({"message": "Switched to graph Database."});
+
+// Create a user for the database above.
+// TODO : find a way to pass the credentials from Docker ENV to this script (if possible).
+db.createUser(
+        {
+            user: "admin",
+            pwd: "admin",
+            roles: [
+                {
+                    role: "readWrite",
+                    db: "graph"
+                }
+            ]
+        }
+);
